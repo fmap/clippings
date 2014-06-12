c079045c888c7f283d04d2cc4a1f1a55e583debf
vi <me@vikramverma.com>
2014-06-12 22:07:29 +0800

    Merge in rejuvyesh's test case.

--- tests/Reader.hs
+++ tests/Reader.hs
@@ -29,6 +29,13 @@ inFixture = Clipping
   , content  = Highlight "Haskell is a great language for constructing code modularly from small but orthogonal building blocks."
   }
 
+inPw2Fixture = Clipping
+  { date     = LocalTime (fromGregorian 2014 06 08) (TimeOfDay 8 36 53)
+  , document = Document "Stand on Zanzibar" (Just "John Brunner")
+  , position = Position Nothing . Just $ Region (4607, 4607)
+  , content  = Highlight "Shinka will"
+  }
+
 getTitle :: Clipping -> String
 getTitle = title . document
 
@@ -40,8 +47,10 @@ main = do
   clipping  <- readFile =<< getDataFileName "tests/fixtures/clipping.txt"
   brackets  <- readFile =<< getDataFileName "tests/fixtures/brackets.txt"
   nested    <- readFile =<< getDataFileName "tests/fixtures/nested_brackets.txt"
+  pw2       <- readFile =<< getDataFileName "tests/fixtures/pw2clipping.txt"
   runAssertions $ 
     [ ("Fixture should parse to sigfpe clipping.", getClipping clipping == inFixture)
     , ("Brackets in clippings' titles should be preserved." , getTitle (getClipping brackets) == "An Introduction to Statistical Learning: with Applications in R (Springer Texts in Statistics)")
     , ("Nested brackets in clippings' authors should be preserved.", getAuthor (getClipping nested) == Just "G. K. (Gilbert Keith) Chesterton")
+    , ("Pw2Fixture should parse to Zanzibar clipping", getClipping pw2 == inPw2Fixture)
     ]
